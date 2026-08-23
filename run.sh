f0b01be8f761ae03086fcff6673e69870f308d24
Your Name <you@example.com>
2026-08-23 12:32:09 +0300

    modify shell

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 cd "$(dirname "$0")"
-python3 -m pip install -r requirements.txt
+
+if [ ! -d "venv" ]; then
+  python3 -m venv venv
+fi
+
+. venv/bin/activate
+
+pip install -r requirements.txt
 python3 server.py
